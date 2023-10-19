3036088adc8641321589de2c4eab12d5a4969330
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-10-19 03:12:29 +0000

    progress

--- app.rb
+++ app.rb
@@ -7,3 +7,30 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  erb(:square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_square = @the_num ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_square_root = @the_num ** 0.5
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:monthly_payment)
+end
