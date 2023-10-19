9598930ea8d725ae704be4dd3dd1a8a704729e78
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-10-19 03:52:29 +0000

    monthly payment

--- app.rb
+++ app.rb
@@ -34,3 +34,22 @@ end
 get("/payment/new") do
   erb(:monthly_payment)
 end
+
+get("/payment/results") do
+  @the_apr = params.fetch("user_apr").to_f.to_fs(:percentage)
+
+  @number_of_years = params.fetch("user_years").to_f
+
+  @the_pv = params.fetch("user_pv").to_f.to_fs(:currency)
+
+  number_of_periods = @number_of_years * 12
+
+  the_rate_per_period = @the_apr.to_f / 1200
+
+  the_numerator = the_rate_per_period.to_f * @the_pv.to_f
+
+  @the_payment = the_numerator
+
+
+  erb(:payment_results)
+end
